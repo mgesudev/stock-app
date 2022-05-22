211fcd41046de1d206091dad2494a2f6f84810b0
Martín Gesualdo <mgesualdo14@gmail.com>
2022-05-22 19:16:40 -0300

    Nuestro primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  console.log('Petición recibida')
+
+  res.status(200).send('<h1>Hola mundo con NODEMON!</h1>')
+})
+
+app.listen(4000, () => {
+  console.log('Servidor escuchando en el puerto 4000')
+})
