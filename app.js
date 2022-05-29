306875d8c8cffebb5b6976262f1a7f49f558801b
Martín <mgesudev@gmail.com>
2022-05-29 18:39:24 -0300

    Conexión con MDB + HTML CSS y JS

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  console.log('Petición recibida')
+mongoose
+  .connect(
+    `mongodb+srv://mgesualdo:${process.env.MONGO_DB_PASS}@development.7fstqap.mongodb.net/stock-app?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT}`)
+    })
+    console.log('Conexión exitosa a la BBDD')
+  })
+  .catch((err) => console.log(err))
 
-  res.status(200).send('<h1>Hola mundo con la nueva cuenta de Azure!</h1>')
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: Number,
+  },
+  { timestamps: true }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+app.use(express.json())
+
+app.post('/api/v1/products', (req, res) => {
+  const newProduct = new Product(req.body)
+
+  newProduct
+    .save()
+    .then((result) => {
+      res.status(201).json({ ok: true })
+    })
+    .catch((err) => console.log(err))
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`)
-})
