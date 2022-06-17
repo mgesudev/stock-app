cfb49ca1baed03102c3a4040374e79351e9e177c
Martín <mgesudev@gmail.com>
2022-06-17 11:20:19 -0300

    Cambios

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
-require('dotenv').config()
 
 const app = express()
 
